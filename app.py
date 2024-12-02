993a4492e0b48e82a22e7f405116cc028f8a899c
saiakshithad <124848030+saiakshithad@users.noreply.github.com>
2024-12-02 15:26:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import random
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 
+
 ssl._create_default_https_context = ssl._create_unverified_context
 nltk.data.path.append(os.path.abspath("nltk_data"))
 nltk.download('punkt')
